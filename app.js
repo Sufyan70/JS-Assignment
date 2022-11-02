31cf383a87a0d76ba8d1a624f7bef2b37efaa13f
Sufyan70 <117269314+Sufyan70@users.noreply.github.com>
2022-11-02 04:54:06 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+// Chapter:01 (alert) 
+alert ("Welcome to First Project of Javascript!");
+window.alert("First Project of Javascript");
+
+// Chapter:02 (Variable for String)
+var abc =  ("Sufyan");
+console.log(abc);
+
+// Chapter:03 (Variable for Number)
+var a = 10;
+var b = 20;
+var c = a+b;
+console.log(c);
+
+var d = 40;
+var e = 100;
+var f = e-d;
+console.log(f);
+
+var f = e*d;
+console.log(f);
+
+var f = d/e;
+console.log(f);
+
+// Chapter:04 (Math expressions Unfamiliar operators)
+//    Post Incriment:-
+var number = 12;
+var newnumber = number++;
+console.log(number);
+  // Post Decrement:-
+ var word = 50;
+ var newword = word--;
+ console.log(word);
+ 
+//   Pre Incriment:-
+var post = 30
+var newpost = ++post;
+console.log(post);
+// Pre Decrement:-
+var pre = 1
+var newpre = --pre;
+console.log(pre);
+
+// Chapter:05 Math expressions Eliminating ambiguity
+var totalcost = 1 + 3 * 5;
+console.log(totalcost);
+ var comput = (2 * 6) * 5 + 4;
+console.log(comput);
+ 
+// Chapter:06 Concatenating text strings
+var username = 2+2;
+console.log(username);
+
+
+
